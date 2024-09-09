c737409e7eb51c2b9aa44accc38e742e5de97f67
Ari Bobesh <ari.bobesh@gmail.com>
2024-09-09 11:27:15 -0400

    Add randomness to send_message.

--- in_class_day02/in_class_day02/send_message.py
+++ in_class_day02/in_class_day02/send_message.py
@@ -1,5 +1,6 @@
 """ This script explores publishing ROS messages in ROS using Python """
 import rclpy
+import random
 from rclpy.node import Node
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point, PointStamped
@@ -18,7 +19,8 @@ class SendMessageNode(Node):
         """Prints a message to the terminal."""
         # print('Hi from in_class_day02.')
         my_header = Header(stamp=self.get_clock().now().to_msg(), frame_id="odom")
-        my_point = Point(x=1.0, y=2.0, z=0.0)
+        my_point = Point(x=6*(random.random()-0.5), y=6*(random.random()-0.5), z=6*(random.random()-0.5))
+        
         my_point_stamped = PointStamped(header=my_header, point=my_point)
         print(my_point_stamped)
         self.publisher.publish(my_point_stamped)
